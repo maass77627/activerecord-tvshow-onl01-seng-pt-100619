15ba5e9838efccf08788a766ce07c60c57801bdb
kelly r maass <kmaass77627@gmail.com>
2020-01-27 23:31:58 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,8 +1,9 @@
 class Show < ActiveRecord::Base
   
   def self.highest_rating 
-  Show.maximum(:rating)
+    Show.maximum(:rating)
   end
-
   
-end
+  def self.most_popular_show  
+    Show.highest_rating 
+  end
